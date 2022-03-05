619b0e938fc13b3f3dfe6dda1b54b50573515062
zy_nitro <zyascend@qq.com>
2022-03-05 23:05:14 +0800

    修改edit

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,50 +1,81 @@
 <template>
-  <div class="container">
-    EDIT
+  <div class="map-wrapper">
+    <mind-map v-model="mapData"></mind-map>
+    <div class="toolbar">
+      <el-tooltip
+        class="tool-item"
+        effect="light"
+        content="适应当前大小"
+        popper-class="popper"
+        placement="left">
+        <div class="" @click="fitView">
+          <img :src="ICON_FITVIEW" alt="">
+        </div>
+      </el-tooltip>
+    </div>
+    <div class="tool-item">
+      <img :src="ICON_THEME" alt="">
+    </div>
   </div>
 </template>
-
 <script>
-import { defineComponent } from 'vue'
-// import { ref, defineComponent, reactive } from 'vue'
-// import { useRouter } from 'vue-router'
-// import { useStore } from 'vuex'
-// import useLogin from '../hooks/useLogin'
+import { defineComponent, onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+import { useZoomMap } from '@/hooks'
+import MindMap from '@/components/MindMap.vue'
+import citys from '@/mock/city'
+import ICON_FITVIEW from '@/assets/pic/fit-view.svg'
+import ICON_THEME from '@/assets/pic/theme.svg'
 
 export default defineComponent({
+  components: {
+    MindMap
+  },
   setup () {
+    const store = useStore()
+    // const mapData = computed(() => store.getters.getColumns)
+    const mapData = computed(() => citys)
+    const fitView = () => {
+      useZoomMap.fitView()
+    }
+    onMounted(() => {
+      store.dispatch('fetchColumns')
+    })
     return {
+      mapData,
+      fitView,
+      ICON_FITVIEW,
+      ICON_THEME
     }
   }
 })
 </script>
-
-<style lang="scss" scoped>
+<style lang="scss">
 @import '../assets/css/mixin';
-.container {
+.map-wrapper {
   @include wh100;
-  background-color: rgba($color: #000000, $alpha: 0.2);
-  .main {
-    @include abs-center;
-    padding: 40px 30px 20px 30px;
-    border-radius: 4px;
-    width: 300px;
-    border-radius: 4px;
-    box-shadow: rgb(0 0 0 / 16%) 0px 2px 30px 0px;
-    background-color: white;
-    box-sizing: border-box;
-    .login-btn{
-      width: 100%;
-      margin-top: 20px;
-    }
-    .title {
-      @include wh100;
-      text-align: center;
-      margin-bottom: 20px;
-      span {
-        color: $color-base;
-      }
+  position: relative;
+  .toolbar {
+    @include vertFlex;
+    position: absolute;
+    z-index: 2;
+    top: 50px;
+    right: 50px;
+    border: 1px solid #dee0e3;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 8px 4px rgba(31, 35, 41, .06);
+    user-select: none;
+    padding: 8px;
+    cursor: pointer;
+    .tool-item + .tool-item {
+      @include wh(16px, 16px);
+      margin-top: 8px;
     }
   }
+}
+.popper {
+  font-size: 10px !important;
+  padding: 6px !important;
 }
 </style>
