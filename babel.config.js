0f4c3f00c48f6464ac73b64f93e962cf40bd0217
zy_nitro <zyascend@qq.com>
2022-05-01 16:54:27 +0800

    【新增】引入Vant 用于构造移动端登录页

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'vant',
+        libraryDirectory: 'es',
+        style: true
+      }
+    ]
   ]
 }
--- src/mlogin/login.vue
+++ src/mlogin/login.vue
@@ -1,7 +1,34 @@
 <template>
-  MOBILE-LOGIN
+  <h1>登录ZMindMap</h1>
+  <Form @submit="onSubmit">
+    <CellGroup inset>
+      <Field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <Field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+    </CellGroup>
+    <div style="margin: 16px;">
+      <Button round block type="primary" native-type="submit">
+        提交
+      </Button>
+    </div>
+  </Form>
 </template>
+<script setup>
+import { Form, Field, CellGroup, Button } from 'vant'
 
+</script>
 <style lang="scss">
 #app {
   width: 100%;
